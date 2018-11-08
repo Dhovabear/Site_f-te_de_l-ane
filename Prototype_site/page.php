312121d583e1ae74df6af5c6aaf12134c0ac5c2a
Dhovabear <powerkiki25000@gmail.com>
2018-11-08 14:00:53 +0100

    ajout du proto
    Ajout des fichier de prototype du site

---
+++ Prototype_site/page.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+
+	<title>La fête de l'âne</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+</head>
+
+
+
+
+
+<body>
+<header>
+<h1>La fête de l'âne</h1>
+</header>
+<aside>
+	<nav>
+		<h3>Accueil</h3>
+		<ul>
+			<li><a href="">Lien 1</a></li>
+			<li><a href="">Lien 2</a></li>
+			<li><a href="">Lien 3</a></li>
+			<li><a href="">Lien 4</a></li>
+		</ul>
+		<h3>Actualités</h3>
+		<ul>
+			<li><a href="">Lien 1</a></li>
+			<li><a href="">Lien 2</a></li>
+			<li><a href="">Lien 3</a></li>
+			<li><a href="">Lien 4</a></li>
+		</ul>
+		<h3>Informations</h3>
+		<ul>
+			<li><a href="">Lien 1</a></li>
+			<li><a href="">Lien 2</a></li>
+			<li><a href="">Lien 3</a></li>
+			<li><a href="">Lien 4</a></li>
+		</ul>
+		<h3>Contact</h3>
+		<ul>
+			<li><a href="">Lien 1</a></li>
+			<li><a href="">Lien 2</a></li>
+			<li><a href="">Lien 3</a></li>
+			<li><a href="">Lien 4</a></li>
+		</ul>
+
+	</nav>
+</aside>
+<main>
+Texte principal de la page<br>
+
+</main>
+
+<footer>
+	
+footer
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
